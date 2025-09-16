b447490b7c0995b7be42452f1518183888acf762
FrancoFabian <jeralfabian489@gmail.com>
2025-09-16 13:35:42 -0600

    chore: migrate project to pnpm and clean webpack config

--- next.config.ts
+++ next.config.ts
@@ -5,88 +5,27 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ['react-icons'],
   },
-  
+
   // Optimización de imágenes
   images: {
     formats: ['image/webp', 'image/avif'],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "ftp3.syscom.mx",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "nextuipro.nyc3.cdn.digitaloceanspaces.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "company.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "support.company.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.pixabay.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.example.com",
-        pathname: "/**",
-      },
+      { protocol: "https", hostname: "ftp3.syscom.mx", pathname: "/**" },
+      { protocol: "https", hostname: "nextuipro.nyc3.cdn.digitaloceanspaces.com", pathname: "/**" },
+      { protocol: "https", hostname: "i.pravatar.cc", pathname: "/**" },
+      { protocol: "https", hostname: "company.com", pathname: "/**" },
+      { protocol: "https", hostname: "support.company.com", pathname: "/**" },
+      { protocol: "https", hostname: "i.imgur.com", pathname: "/**" },
+      { protocol: "https", hostname: "images.pexels.com", pathname: "/**" },
+      { protocol: "https", hostname: "cdn.pixabay.com", pathname: "/**" },
+      { protocol: "https", hostname: "cdn.shopify.com", pathname: "/**" },
+      { protocol: "https", hostname: "www.example.com", pathname: "/**" },
     ],
   },
 
   // Optimización de compilación
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
-  },
-
-  // Optimización de bundle
-  webpack: (config) => {
-    // Optimizaciones generales
-    config.optimization = {
-      ...config.optimization,
-      splitChunks: {
-        ...config.optimization.splitChunks,
-        cacheGroups: {
-          ...config.optimization.splitChunks?.cacheGroups,
-          icons: {
-            name: 'icons',
-            test: /[\\/]node_modules[\\/]react-icons[\\/]/,
-            chunks: 'all',
-            priority: 10,
-          },
-        },
-      },
-    };
-    return config;
   },
 };
 
